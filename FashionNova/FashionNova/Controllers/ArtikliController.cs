4e2d1cb391044b2b2b412f8d962de3bf453ba904
Belma Nukić <nukicbelma@hotmail.com>
2021-11-12 23:12:03 +0100

    ArtikliController, service, interface

---
+++ FashionNova/FashionNova/Controllers/ArtikliController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionNova.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class ArtikliController :ControllerBase
+    {
+
+    }
+}
